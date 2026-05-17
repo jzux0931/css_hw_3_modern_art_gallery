d3c3556d62388e17b7a0246e05367d824eb3b011
Jzux <jmgc.0931@gmail.com>
2026-05-17 16:51:16 -0600

    Fix Github Pages multi-page build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   base: '/css_hw_3_modern_art_gallery/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        location: resolve(process.cwd(), 'location.html'),
+      },
+    },
+  },
 });
